aa471613719ffbaab1bda6e965f018ae0e48138e
xjaioajcow <42373988+xjaioajcow@users.noreply.github.com>
2025-06-05 04:12:05 +0800

    修改 hardhat.config.js：改为本地 soljson.js 路径

--- hardhat.config.js
+++ hardhat.config.js
@@ -4,14 +4,15 @@ const { subtask } = require("hardhat/config");
 const { TASK_COMPILE_SOLIDITY_GET_SOLC_BUILD } = require("hardhat/builtin-tasks/task-names");
 const path = require("path");
 
+// 覆盖默认下载任务，直接指向本地 soljson.js
 subtask(TASK_COMPILE_SOLIDITY_GET_SOLC_BUILD, async (args, hre, runSuper) => {
   if (args.solcVersion === "0.8.20") {
-    const compilerPath = path.join(__dirname, "node_modules/solc/soljson-v0.8.20+commit.a1b79de6.js");
+    const compilerPath = path.join(__dirname, "node_modules/solc/soljson.js");
     return {
       compilerPath,
       isSolcJs: true,
       version: args.solcVersion,
-      longVersion: "0.8.20+commit.a1b79de6"
+      longVersion: "0.8.20+commit"
     };
   }
   return runSuper();
