74c97679983ecadfd37c2eb3bafe888c31b94f26
xjaioajcow <42373988+xjaioajcow@users.noreply.github.com>
2025-06-05 03:06:13 +0800

    Update hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,13 +3,18 @@ require("dotenv").config();
 
 module.exports = {
   solidity: {
-    version: "0.8.20",
-    settings: {
-      optimizer: {
-        enabled: true,
-        runs: 200
+    compilers: [
+      {
+        version: "0.8.20",
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 200
+          }
+        }
       }
-    }
+    ],
+    useSolcJs: true
   },
   networks: {
     hyperevm: {
