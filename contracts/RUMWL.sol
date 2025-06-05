54a34c597f0b0c0d87a84db8a1615da6b20eaec9
xjaioajcow <42373988+xjaioajcow@users.noreply.github.com>
2025-06-05 16:29:07 +0800

    Update RUMWL.sol

--- contracts/RUMWL.sol
+++ contracts/RUMWL.sol
@@ -8,7 +8,19 @@ import "@openzeppelin/contracts/access/Ownable.sol";
 /// @notice 只有 owner 可以 mint，每次 mint 时 tokenId = round，可用于白名单、奖品等
 /// @dev 合约名称为 RUMWL，代币符号为 RUMWL
 contract RUMWL is ERC721, Ownable {
-    constructor() ERC721("RUM-WL", "RUMWL") {}
+    string private _baseTokenURI;
+
+    constructor(string memory baseURI) ERC721("RUM-WL", "RUMWL") {
+        _baseTokenURI = baseURI;
+    }
+
+    function setBaseURI(string calldata baseURI) external onlyOwner {
+        _baseTokenURI = baseURI;
+    }
+
+    function _baseURI() internal view override returns (string memory) {
+        return _baseTokenURI;
+    }
 
     /// @notice 铸造一个 NFT，只有 owner 可以操作
     /// @param to 接收地址
