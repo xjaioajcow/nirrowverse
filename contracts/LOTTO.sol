c2c6a82c977dd30e832838edb72590e00d598801
xjaioajcow <42373988+xjaioajcow@users.noreply.github.com>
2025-06-05 13:16:55 +0800

    添加 contracts/LOTTO.sol

--- contracts/LOTTO.sol
+++ contracts/LOTTO.sol
@@ -1,1 +1,17 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
 
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
+
+contract LOTTO is ERC20, Ownable {
+    constructor() ERC20("LOTTO", "LOTTO") {}
+
+    function decimals() public pure override returns (uint8) {
+        return 18;
+    }
+
+    function mint(address to, uint256 amount) external onlyOwner {
+        _mint(to, amount);
+    }
+}    
